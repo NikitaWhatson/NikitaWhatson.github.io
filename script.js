fc9781d195f24b2a444966ad05deb2f6d14c09e5
NikitaWhatson <49592163+NikitaWhatson@users.noreply.github.com>
2019-05-28 23:07:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+menu_btn.addEventListener('click', function(){
+  main_nav.classList.toggle('menu_is-open');
+  Hlink.classList.toggle('title-close');
+  Hlink1.classList.toggle('title-close');
+  Hlink2.classList.toggle('title-close');
+  Hlink3.classList.toggle('title-close');
+})
