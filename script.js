445ac701124651a5c9a50bfb9107b575771a0a3a
NikitaWhatson <49592163+NikitaWhatson@users.noreply.github.com>
2019-06-01 20:40:33 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-menu_btn.addEventListener('click', function(){
-  main_nav.classList.toggle('menu_is-open');
-  Hlink.classList.toggle('title-close');
-  Hlink1.classList.toggle('title-close');
-  Hlink2.classList.toggle('title-close');
-  Hlink3.classList.toggle('title-close');
-})
